84835a2bd5d52bc8dc9d1b87aaad2c2d8e3430a6
Zackery Aguirre <70052175+Zackaguirre97@users.noreply.github.com>
2025-10-16 08:43:24 -0500

    Enhance prompts, calculate balance in start loop, add random deposit generator, populate deposit details

--- src/main/java/com/pluralsight/Controller.java
+++ src/main/java/com/pluralsight/Controller.java
@@ -1,6 +1,7 @@
 package com.pluralsight;
 
 import java.io.IOException;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Controller {
@@ -21,13 +22,13 @@ public class Controller {
     // Print statements for the main menu/starting loop
     public void showMainMenu() {
         // Display the main menu
-        System.out.println("\n--- Main Menu ---");
+        System.out.println("\n*** Main Menu ***");
         System.out.println("D. Add Deposit");
         System.out.println("P. Make Payment");
         System.out.println("L. Ledger");
         System.out.println("X. Exit");
         // Prompt the user to enter a choice from the menu.
-        System.out.println("Select an option: ");
+        System.out.print("Select an option: ");
     }
 
     // Starting menu/page. Shows the main menu
@@ -35,6 +36,8 @@ public class Controller {
         boolean runAgain = true;
         // Loop until the user chooses to exit
         while(runAgain) {
+            // Calculate the balance.
+            account.calculateBalance();
             // Call/Display the main menu
             showMainMenu();
             // Each loop reset main menu choice input variable.
@@ -65,14 +68,15 @@ public class Controller {
     public void addDeposit() {
         boolean runAgain = true;
         while (runAgain) {
-            System.out.println("\n--- Make Deposit ---");
-            System.out.println("Enter the deposit amount: ");
+            System.out.println("\n=== Make Deposit ===");
+            System.out.print("Enter the deposit amount: ");
             String depositAmountInput = sc.nextLine().trim();
             // Once verifying that the depositAmount is a double- continue.
             if (isDouble(depositAmountInput)) {
                 double depositAmount = Double.parseDouble(depositAmountInput);
-                String depositVendor = "Bank";
-                String depositDescription = "Deposit";
+                String[] randomDepositInfo = getRandomDepositInfo();
+                String depositDescription = randomDepositInfo[0];
+                String depositVendor = randomDepositInfo[1];
                 Transaction newDeposit = new Transaction(depositDescription, depositVendor, depositAmount);
                 account.addTransaction(newDeposit);
                 fileManager.updateTransactionsFile(account.getTransactions());
@@ -89,18 +93,19 @@ public class Controller {
     public void makePayment() {
         boolean runAgain = true;
         while (runAgain) {
-            System.out.println("\n--- Make Payment ---");
-            System.out.println("Enter the payment amount: ");
+            System.out.println("\n=== Make Payment ===");
+            System.out.print("Enter the payment amount: ");
             String paymentAmountInput = sc.nextLine().trim();
             // Verify that the amount entered is a valid double
             if (isDouble(paymentAmountInput)) {
                 double paymentAmount = Double.parseDouble(paymentAmountInput);
-                System.out.println("Enter the description: ");
+                System.out.print("Enter the description: ");
                 String paymentDescription = sc.nextLine().trim();
-                System.out.println("Enter the vendor name: ");
+                System.out.print("Enter the vendor name: ");
                 String paymentVendor = sc.nextLine().trim();
                 Transaction newPayment = new Transaction(paymentDescription, paymentVendor, (paymentAmount * -1));
                 account.addTransaction(newPayment);
+                fileManager.updateTransactionsFile(account.getTransactions());
                 System.out.println("Confirmation: Payment completed");
                 runAgain = false;
             } // If the value entered was not a double
@@ -112,14 +117,14 @@ public class Controller {
 
     // Handles the display of the ledger menu
     public void displayLedgerMenu() {
-        System.out.println("\n--- Ledger Menu ---");
+        System.out.println("\n=== Ledger Menu ===");
         System.out.println("A. All Transactions");
         System.out.println("D. Deposits Only");
         System.out.println("P. Payments Only");
         System.out.println("R. Reports");
         System.out.println("H. Home");
         // Prompt the user to enter a choice from the above menu.
-        System.out.println("Select an option: ");
+        System.out.print("Select an option: ");
     }
 
     // Handles the ledger page logic/flow
@@ -133,6 +138,7 @@ public class Controller {
                 case "D":
                 case "P":
                     if (account.getReport(ledgerMenuChoice)) {
+                        break;
                     }
                     else {
                         if (ledgerMenuChoice.equalsIgnoreCase("A")) {
@@ -158,7 +164,7 @@ public class Controller {
 
     // Handles the display of the reports menu
     public void showReportsMenu() {
-        System.out.println("\n--- Reports Menu ---");
+        System.out.println("\n=== Reports Menu ===");
         System.out.println("1. Month To Date");
         System.out.println("2. Previous Month");
         System.out.println("3. Year To Date");
@@ -166,7 +172,7 @@ public class Controller {
         System.out.println("5. Search by Vendor");
         System.out.println("0. Back");
         // Prompt the user to enter a choice from the above menu.
-        System.out.println("Select an option: ");
+        System.out.print("Select an option: ");
     }
 
     // Handles the reports page logic/flow
@@ -187,7 +193,7 @@ public class Controller {
                         account.getReport(reportsMenuChoice);
                         break;
                     case "5":
-                        System.out.println("Please enter a vendor name:");
+                        System.out.print("Please enter a vendor name:");
                         String vendor = sc.nextLine().trim();
                         account.getReport(vendor);
                         break;
@@ -200,6 +206,41 @@ public class Controller {
                 System.out.println("Please enter a whole number.");
             }
         }
+    }
+
+    // Method to generate random descriptions and vendors for the deposit transactions.
+    public String[] getRandomDepositInfo() {
+        String[] defaultDescriptions = {
+                "Deposit - Refueling Contract",
+                "Deposit - Delivery Refund",
+                "Deposit - Merchant Account Sync",
+                "Deposit - Fleet Rebate",
+                "Deposit - Galactic Express Payment",
+                "Deposit - Station Bonus Payment",
+                "Deposit - Cargo Delivery Refund",
+                "Deposit - Interstellar Trade Credit",
+                "Deposit - Rebate Credit",
+                "Deposit - Cosmic Trade Settlement"
+        };
+        String[] defaultVendors = {
+                "Orion Fleet Co.",
+                "Quantum Traders",
+                "Celestial Systems",
+                "Nova Credit Union",
+                "Galactic Express",
+                "Cosmic Trade Bank",
+                "Lunar Freight",
+                "Crimson Convoy",
+                "Nova Credit Union",
+                "Cosmic Trade Bank"
+        };
+
+        // Generate a random number
+        Random rand = new Random();
+        int description = rand.nextInt(defaultDescriptions.length);
+        int vendor = rand.nextInt(defaultVendors.length);
+
+        return new String[]{defaultDescriptions[description], defaultVendors[vendor]};
     }
 
     // Check if the passed string is an integer.
